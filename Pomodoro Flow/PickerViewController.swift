300d29b6abe19779f89ed43d9b7a89afb72005a1
Dan K <itsdanya@gmail.com>
2016-04-06 17:33:59 +0300

    Update pomodoro length options

--- Pomodoro Flow/PickerViewController.swift
+++ Pomodoro Flow/PickerViewController.swift
@@ -23,7 +23,7 @@ class PickerViewController: UITableViewController {
     var delegate: PickerViewControllerDelegate?
 
     private struct PickerOptions {
-        static let pomodoroLength = [1, 25, 30, 35, 40].map { $0 * 60 }
+        static let pomodoroLength = [25, 30, 35, 40].map { $0 * 60 }
         static let shortBreakLength = [5, 10, 15, 20].map { $0 * 60 }
         static let longBreakLength = [10, 15, 20, 25, 30].map { $0 * 60 }
         static let targetPomodoros = (2...14).map { $0 }
