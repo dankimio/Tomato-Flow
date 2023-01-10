b78be9846e1811be17e29d4e6f4f843b94049766
Dan Kim <itsdanya@gmail.com>
2023-01-10 18:36:13 +0000

    Update cell color

--- Pomodoro Flow/MyCell.swift
+++ Pomodoro Flow/MyCell.swift
@@ -1,19 +1,11 @@
-//
-//  File.swift
-//  Pomodoro Flow
-//
-//  Created by Dan on 10/01/2023.
-//  Copyright © 2023 Dan K. All rights reserved.
-//
-
 import UIKit
 
 class MyCell: UICollectionViewCell {
-    override func layoutSubviews() {
-        super.layoutSubviews()
-        
-        contentView.backgroundColor = .red
-        contentView.layer.cornerRadius = min(contentView.bounds.width, contentView.bounds.height) / 2.0
-        contentView.layer.masksToBounds = true
-    }
+  override func layoutSubviews() {
+    super.layoutSubviews()
+    
+    contentView.backgroundColor = Colors.primary
+    contentView.layer.cornerRadius = min(contentView.bounds.width, contentView.bounds.height) / 2.0
+    contentView.layer.masksToBounds = true
+  }
 }
