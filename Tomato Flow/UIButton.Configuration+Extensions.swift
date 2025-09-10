b72d1bf4250f9a0610c647435cd05c66d528a3f9
Dan Kim <git@dan.kim>
2025-09-11 00:22:26 +0300

    Use medium font weight in buttons

--- Tomato Flow/UIButton.Configuration+Extensions.swift
+++ Tomato Flow/UIButton.Configuration+Extensions.swift
@@ -4,11 +4,28 @@ extension UIButton.Configuration {
   public static func primary() -> UIButton.Configuration {
     var configuration = UIButton.Configuration.filled()
     configuration.baseBackgroundColor = Colors.primary
+    configuration.titleTextAttributesTransformer = UIConfigurationTextAttributesTransformer {
+      incoming in
+      var outgoing = incoming
+      let bodyFont = UIFont.preferredFont(forTextStyle: .body)
+      let mediumWeight = UIFont.Weight.medium
+      outgoing.font = UIFont.systemFont(ofSize: bodyFont.pointSize, weight: mediumWeight)
+      return outgoing
+    }
 
     return configuration
   }
 
   public static func secondary() -> UIButton.Configuration {
-    return UIButton.Configuration.tinted()
+    var configuration = UIButton.Configuration.tinted()
+    configuration.titleTextAttributesTransformer = UIConfigurationTextAttributesTransformer {
+      incoming in
+      var outgoing = incoming
+      let bodyFont = UIFont.preferredFont(forTextStyle: .body)
+      let mediumWeight = UIFont.Weight.medium
+      outgoing.font = UIFont.systemFont(ofSize: bodyFont.pointSize, weight: mediumWeight)
+      return outgoing
+    }
+    return configuration
   }
 }
