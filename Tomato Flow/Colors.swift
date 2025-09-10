cd874dc78b7cfff35f6bb81cf710f410f6d47b94
Dan Kim <git@dan.kim>
2025-09-11 00:59:50 +0300

    lint

--- Tomato Flow/Colors.swift
+++ Tomato Flow/Colors.swift
@@ -1,5 +1,5 @@
 import UIKit
 
 struct Colors {
-  static let primary = UIColor(red: 240/255, green: 90/255, blue: 90/255, alpha: 1)
+  static let primary = UIColor(red: 240 / 255, green: 90 / 255, blue: 90 / 255, alpha: 1)
 }
