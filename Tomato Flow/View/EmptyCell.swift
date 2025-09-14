ab6978c7857b5c8c8f5998f08ba03f2057f707ef
Dan Kim <git@dan.kim>
2025-09-14 03:37:58 +0300

    revert system gray for empty cells

--- Tomato Flow/View/EmptyCell.swift
+++ Tomato Flow/View/EmptyCell.swift
@@ -7,7 +7,6 @@ class EmptyCell: UICollectionViewCell {
     contentView.layer.cornerRadius = min(contentView.bounds.width, contentView.bounds.height) / 2.0
     contentView.layer.masksToBounds = true
     contentView.layer.borderWidth = 2.5
-    // Use separator color which adapts to dark/light appearances
-    contentView.layer.borderColor = UIColor.separator.cgColor
+    contentView.layer.borderColor = UIColor.systemGray4.cgColor
   }
 }
